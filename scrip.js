cff2f59db7e0224d718e27850f94ddcde2120cd4
Cappuh <140728514+carolinamatosdacosta@users.noreply.github.com>
2024-11-29 11:19:55 -0300

    socorro

--- scrip.js
+++ scrip.js
@@ -1,1 +1,13 @@
+const text = "Este é o texto que será digitado.";
+const typing = document.querySelector('.typing span');
+let textIndex = 0;
 
+function type() {
+    typing.textContent += text.charAt(textIndex);
+    textIndex++;
+    if (textIndex === text.length) {
+        // Aqui você pode adicionar outras ações após a digitação terminar
+    }
+}
+
+setInterval(type, 100); // Ajuste o intervalo para controlar a velocidade
