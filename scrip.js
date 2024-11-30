4078a3d8270094e6bb357cbbc64b83f43ad3ad4c
Cappuh <140728514+carolinamatosdacosta@users.noreply.github.com>
2024-11-30 17:01:07 -0300

    blabla

--- scrip.js
+++ scrip.js
@@ -6,8 +6,7 @@ function type() {
     typing.textContent += text.charAt(textIndex);
     textIndex++;
     if (textIndex === text.length) {
-        // Aqui você pode adicionar outras ações após a digitação terminar
     }
 }
 
-setInterval(type, 100); // Ajuste o intervalo para controlar a velocidade
+setInterval(type, 100); /
